dbed645c7745e71937f3e20ac52974191c51a8f1
박혜민 <hmpkk09@gmail.com>
2023-04-08 16:35:11 +0900

    move CurrencySelector

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,10 +21,10 @@ import {
   QuestionMarkCircleIcon,
   ShoppingBagIcon,
 } from "@heroicons/react/24/outline";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import { atom, useSetAtom } from "jotai";
 import { MobileMenu } from "@/components/MobileMenu";
 import { DecorativeImage } from "@/components/DecorativeImage";
+import { CurrencySelector } from "@/components/CurrencySelector";
 
 export type Navigation = {
   categories: {
@@ -218,34 +218,6 @@ export const mobileMenuOpenAtom = atom(false);
 export const classNames = (...classes: string[]) =>
   classes.filter(Boolean).join(" ");
 
-const CurrencySelector = () => {
-  return (
-    <form>
-      <div>
-        <label htmlFor="desktop-currency" className="sr-only">
-          Currency
-        </label>
-        <div className="group relative -ml-2 rounded-md border-transparent bg-gray-900 focus-within:ring-2 focus-within:ring-white">
-          <select
-            id="desktop-currency"
-            name="currency"
-            className="flex items-center rounded-md border-transparent bg-gray-900 bg-none py-0.5 pl-2 pr-5 text-sm font-medium text-white focus:border-transparent focus:outline-none focus:ring-0 group-hover:text-gray-100"
-          >
-            {currencies.map((currency) => (
-              <option key={currency}>{currency}</option>
-            ))}
-          </select>
-          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center">
-            <ChevronDownIcon
-              className="h-5 w-5 text-gray-300"
-              aria-hidden="true"
-            />
-          </div>
-        </div>
-      </div>
-    </form>
-  );
-};
 export default function Example() {
   const setMobileMenuOpen = useSetAtom(mobileMenuOpenAtom);
 
@@ -263,7 +235,7 @@ export default function Example() {
             <div className="bg-gray-900">
               <div className="mx-auto flex h-10 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
                 {/* Currency selector */}
-                <CurrencySelector />
+                <CurrencySelector currencies={currencies} />
 
                 <div className="flex items-center space-x-6">
                   <a
