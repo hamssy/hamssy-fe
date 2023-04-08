9d523eaf841ea2f65a26903f78063588eee91417
박혜민 <hmpkk09@gmail.com>
2023-04-08 16:33:21 +0900

    extract CurrencySelector

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,6 +24,7 @@ import {
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import { atom, useSetAtom } from "jotai";
 import { MobileMenu } from "@/components/MobileMenu";
+import { DecorativeImage } from "@/components/DecorativeImage";
 
 export type Navigation = {
   categories: {
@@ -217,21 +218,32 @@ export const mobileMenuOpenAtom = atom(false);
 export const classNames = (...classes: string[]) =>
   classes.filter(Boolean).join(" ");
 
-const DecorativeImage = () => {
+const CurrencySelector = () => {
   return (
-    <>
-      <div aria-hidden="true" className="absolute inset-0 overflow-hidden">
-        <img
-          src="https://tailwindui.com/img/ecommerce-images/home-page-01-hero-full-width.jpg"
-          alt=""
-          className="h-full w-full object-cover object-center"
-        />
+    <form>
+      <div>
+        <label htmlFor="desktop-currency" className="sr-only">
+          Currency
+        </label>
+        <div className="group relative -ml-2 rounded-md border-transparent bg-gray-900 focus-within:ring-2 focus-within:ring-white">
+          <select
+            id="desktop-currency"
+            name="currency"
+            className="flex items-center rounded-md border-transparent bg-gray-900 bg-none py-0.5 pl-2 pr-5 text-sm font-medium text-white focus:border-transparent focus:outline-none focus:ring-0 group-hover:text-gray-100"
+          >
+            {currencies.map((currency) => (
+              <option key={currency}>{currency}</option>
+            ))}
+          </select>
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center">
+            <ChevronDownIcon
+              className="h-5 w-5 text-gray-300"
+              aria-hidden="true"
+            />
+          </div>
+        </div>
       </div>
-      <div
-        aria-hidden="true"
-        className="absolute inset-0 bg-gray-900 opacity-50"
-      />
-    </>
+    </form>
   );
 };
 export default function Example() {
@@ -251,30 +263,7 @@ export default function Example() {
             <div className="bg-gray-900">
               <div className="mx-auto flex h-10 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
                 {/* Currency selector */}
-                <form>
-                  <div>
-                    <label htmlFor="desktop-currency" className="sr-only">
-                      Currency
-                    </label>
-                    <div className="group relative -ml-2 rounded-md border-transparent bg-gray-900 focus-within:ring-2 focus-within:ring-white">
-                      <select
-                        id="desktop-currency"
-                        name="currency"
-                        className="flex items-center rounded-md border-transparent bg-gray-900 bg-none py-0.5 pl-2 pr-5 text-sm font-medium text-white focus:border-transparent focus:outline-none focus:ring-0 group-hover:text-gray-100"
-                      >
-                        {currencies.map((currency) => (
-                          <option key={currency}>{currency}</option>
-                        ))}
-                      </select>
-                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center">
-                        <ChevronDownIcon
-                          className="h-5 w-5 text-gray-300"
-                          aria-hidden="true"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                </form>
+                <CurrencySelector />
 
                 <div className="flex items-center space-x-6">
                   <a
