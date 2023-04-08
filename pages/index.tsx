b4c4037ff6be8d06f2f7ea290946493d97682a35
박혜민 <hmpkk09@gmail.com>
2023-04-08 17:25:38 +0900

    move Introduction

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,8 @@
-/*
-  This example requires some changes to your config:
-
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-      require('@tailwindcss/aspect-ratio'),
-    ],
-  }
-  ```
-*/
 import { atom } from "jotai";
 import { MobileMenu } from "@/components/MobileMenu";
 import { DecorativeImage } from "@/components/DecorativeImage";
 import { Header } from "@/components/Header";
+import { Introduction } from "@/components/Introduction";
 
 export type Navigation = {
   categories: {
@@ -218,23 +204,7 @@ const Home = () => {
       <div className="relative bg-gray-900">
         <DecorativeImage />
         <Header navigation={navigation} currencies={currencies} />
-
-        <div className="relative mx-auto flex max-w-3xl flex-col items-center px-6 py-32 text-center sm:py-64 lg:px-0">
-          <h1 className="text-4xl font-bold tracking-tight text-white lg:text-6xl">
-            New arrivals are here
-          </h1>
-          <p className="mt-4 text-xl text-white">
-            The new arrivals have, well, newly arrived. Check out the latest
-            options from our summer small-batch release while they're still in
-            stock.
-          </p>
-          <a
-            href="#"
-            className="mt-8 inline-block rounded-md border border-transparent bg-white px-8 py-3 text-base font-medium text-gray-900 hover:bg-gray-100"
-          >
-            Shop New Arrivals
-          </a>
-        </div>
+        <Introduction />
       </div>
 
       <main>
