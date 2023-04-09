cc1389c732b255a804006725cb395c7f7e397a9b
박혜민 <hmpkk09@gmail.com>
2023-04-09 14:42:41 +0900

    extract Product

---
+++ components/Product.tsx
@@ -0,0 +1,21 @@
+type Props = {
+  id: string | number;
+  imageSrc: string;
+  name: string;
+  price: number;
+};
+
+export const Product = ({ id, imageSrc, name, price }: Props) => {
+  return (
+    <a key={id} href="" className="group">
+      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+        <img
+          src={imageSrc}
+          className="h-full w-full object-cover object-center group-hover:opacity-75"
+        />
+      </div>
+      <h3 className="mt-4 text-sm text-gray-700">{name}</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">{price}</p>
+    </a>
+  );
+};
